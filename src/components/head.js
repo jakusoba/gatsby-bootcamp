040a0873d3b7948ff6926a83fa64008f8bbdb513
jakusoba <jane.akusoba@gmail.com>
2021-04-23 13:46:59 -0400

    commented out header

--- src/components/head.js
+++ src/components/head.js
@@ -3,20 +3,20 @@ import { Helmet } from 'react-helmet'
 import {useStaticQuery, graphql } from 'gatsby'
 
 
-const Head = ({ title}) => {
-    const data = useStaticQuery(graphql`
-        query {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `)
-    return (
-        <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
+//const Head = ({ title}) => {
+    //const data = useStaticQuery(graphql`
+        //query {
+            //site {
+                //siteMetadata {
+                    //title
+                //}
+           // }
+        //}
+   // `)
+    //return (
+       // <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />
 
-    )
-}
+   // )
+//}
 
-export default Head 
+//export default Head 
--- src/components/header.js
+++ src/components/header.js
@@ -3,43 +3,43 @@ import { Link, graphql, useStaticQuery } from 'gatsby'
 
 import headerStyles from './header.module.scss'
 
-const Header = () => {
-   const data = useStaticQuery(graphql`
-     query {
-       site {
+//const Header = () => {
+   //const data = useStaticQuery(graphql`
+    // query {
+       //site {
          
-        }
-      }
+        //}
+      //}
    
-   `)    
+   //`)    
 
 
-    return (
-        <header className={headerStyles.header}>
-            <h1>
+    //return (
+        //<header className={headerStyles.header}>
+           // <h1>
                
-                </h1>
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
-                    </li>  
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+                //</h1>
+            //<nav>
+                //<ul className={headerStyles.navList}>
+                    //<li>
+                        //<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
+                    //</li>
+                    //<li>
+                        //<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
+                    //</li>
+                    //<li>
+                        //<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
+                    //</li>  
+                    //<li>
+                        //<Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link>
+                    //</li>
+               // </ul>
+            //</nav>
+       // </header>
+    /////)
+//}
 
-export default Header
+//export default Header
 
 //This was copied from line 8
 //query {
